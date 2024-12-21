d282690e7b4a36e5b33ef35203987521e1d754b6
Marcus Roland <marcus.roland@gmail.com>
2024-12-21 17:47:37 -0300

    ajustes na importacao para a interface

--- src/java/br/com/atarde/wsservicosap/dao/ItemDAO.java
+++ src/java/br/com/atarde/wsservicosap/dao/ItemDAO.java
@@ -8,24 +8,24 @@ import br.com.topsys.database.factory.TSDataBaseBrokerFactory;
 
 public class ItemDAO {
 
-	public ItemModel obter(ItemModel model) {
+    public ItemModel obter(ItemModel model) {
 
         TSDataBaseBrokerIf broker = TSDataBaseBrokerFactory.getDataBaseBrokerIf(model.getEmpresaModel().getJndi());
 
-        broker.setSQL("SELECT OITM.\"ItemCode\", OITM.\"InvntItem\", OITM.\"SellItem\", OITM.\"PrchseItem\" FROM "+ model.getEmpresaModel().getDbInstancia() + ".OITM WHERE OITM.\"ItemCode\" = ?", model.getId());
+        broker.setSQL("SELECT OITM.\"ItemCode\", CASE WHEN OITM.\"InvntItem\" = 'Y' THEN TRUE ELSE FALSE END, CASE WHEN OITM.\"SellItem\" = 'Y' THEN TRUE ELSE FALSE END, CASE WHEN OITM.\"PrchseItem\" = 'Y' THEN TRUE ELSE FALSE END FROM " + model.getEmpresaModel().getDbInstancia() + ".OITM WHERE OITM.\"ItemCode\" = ?", model.getId());
 
         return (ItemModel) broker.getObjectBean(ItemModel.class, "id", "flagControleEstoque", "flgItemVenda", "flagItemCompra");
-        
-	}
 
-	@SuppressWarnings("unchecked")
-	public List<ItemModel> pesquisar(ItemModel model) {
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<ItemModel> pesquisar(ItemModel model) {
 
         TSDataBaseBrokerIf broker = TSDataBaseBrokerFactory.getDataBaseBrokerIf(model.getEmpresaModel().getJndi());
 
         broker.setSQL("SELECT ITEM.\"ItemCode\" AS ID_EXTERNO, ITEM.\"ItemName\" AS DESCRICAO, PRECO.\"Price\" AS VALOR_UNITARIO , LINHA.\"OnHand\" AS QTD_DISPONIVEL FROM " + model.getEmpresaModel().getDbInstancia() + ".OITM ITEM , " + model.getEmpresaModel().getDbInstancia() + ".ITM1 PRECO, " + model.getEmpresaModel().getDbInstancia() + ".OITW AS LINHA WHERE ITEM.\"ItemCode\" = PRECO.\"ItemCode\" AND LINHA.\"ItemCode\" = ITEM.\"ItemCode\" AND PRECO.\"PriceList\" = 3 AND LINHA.\"OnHand\" > 0 AND LINHA.\"WhsCode\" = ? ", model.getEstoqueModel().getId());
 
         return broker.getCollectionBean(ItemModel.class, "id", "descricao", "preco", "estoqueModel.quantidadeDisponivel");
-	}
+    }
 
 }
--- src/java/br/com/atarde/wsservicosap/dao/NotaFiscalSaidaDAO.java
+++ src/java/br/com/atarde/wsservicosap/dao/NotaFiscalSaidaDAO.java
@@ -12,7 +12,7 @@ public class NotaFiscalSaidaDAO {
 
         TSDataBaseBrokerIf broker = TSDataBaseBrokerFactory.getDataBaseBrokerIf(model.getEmpresaModel().getJndi());
 
-        broker.setSQL("SELECT OINV.\"DocEntry\" FROM " + model.getEmpresaModel().getDbInstancia() + ".OINV WHERE OINV.\"NumAtCard\" = ? AND OINV.\"U_ATRD_Origem\"= ? AND NOT EXISTS (SELECT 1 FROM " + model.getEmpresaModel().getDbInstancia() + ".ORIN, " + model.getEmpresaModel().getDbInstancia() + ".RIN1 WHERE ORIN.\"DocEntry\" = RIN1.\"DocEntry\" AND ORIN.\"SeqCode\" IN(1,28) AND RIN1.\"BaseEntry\" = OINV.\"DocEntry\") AND NOT EXISTS (SELECT 1 FROM " + model.getEmpresaModel().getDbInstancia() + ".OINV C, " + model.getEmpresaModel().getDbInstancia() + ".INV1 L WHERE C.\"DocEntry\" = L.\"DocEntry\" AND L.\"BaseEntry\" = OINV.\"DocEntry\" AND C.CANCELED = 'Y') AND OINV.CANCELED ='N' ", model.getIdExterno(), model.getOrigemModel().getId());
+        broker.setSQL("SELECT OINV.\"DocEntry\" FROM " + model.getEmpresaModel().getDbInstancia() + ".OINV WHERE OINV.\"NumAtCard\" = ? AND OINV.\"U_ATRD_Origem\"= ? AND NOT EXISTS (SELECT 1 FROM " + model.getEmpresaModel().getDbInstancia() + ".ORIN, " + model.getEmpresaModel().getDbInstancia() + ".RIN1 WHERE ORIN.\"DocEntry\" = RIN1.\"DocEntry\" AND ORIN.\"SeqCode\" IN(1,27,28,29,31,32,33,35) AND RIN1.\"BaseEntry\" = OINV.\"DocEntry\") AND NOT EXISTS (SELECT 1 FROM " + model.getEmpresaModel().getDbInstancia() + ".OINV C, " + model.getEmpresaModel().getDbInstancia() + ".INV1 L WHERE C.\"DocEntry\" = L.\"DocEntry\" AND L.\"BaseEntry\" = OINV.\"DocEntry\" AND C.CANCELED = 'Y') AND OINV.CANCELED ='N' ", model.getIdExterno(), model.getOrigemModel().getId());
 
         return (NotaFiscalSaidaModel) broker.getObjectBean(NotaFiscalSaidaModel.class, "id");
 
